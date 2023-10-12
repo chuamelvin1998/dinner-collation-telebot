3fda172d581fc4c3155d18105c5a09903f80504e
Melvin Chua <melvin.chua@dyson.com>
2023-10-13 00:47:38 +0800

    explore polling on telegram, rough plan on README

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # dinner-collation-telebot
+## Background
+
+As one of my chores is to cook dinner for my family. I have to know how many of my family members are coming back to eat dinner, so i can prepare the amount of food correctly and reduce wastage and effort.
+
+I created this bot to help solve this hassle of checking my family members on who is coming back for dinner.
+
+## Problem
+My family members do not always notified the chat group that they are coming back for dinner or they are not.
+
+Thus, i have to call them and ask them which is a hassle.
+
+# User Stories
+
+/help
+
+/start
+ - run /settings
+
+1. let the user choose which day to monitor, MONDAY, TUES, WED ETC
+2. let the user choose which time to start monitoring, 6pm onwards
+3. let the user choose which time to stop monitoring, 7.30pm onwards ---- 6pm to 7.30pm
+
+/poll
+- run 2 polls at the start of monitoring
+- notify the user that have not poll yet in a fixed interval?? before the time stop 
+
+## poll 1 
+who is coming back for dinner? coming back for dinner | not me | im cooking dinner 
+
+## give conclusion
+at the end of time stop, 
+### scenario
+1. if no one is cooking dinner - THERE IS NO DINNER AT HOME, GO EAT OUTSIDE
+2. if 1 or 2 is cooking dinner at home, give the total number of people eating dinner  - THERE IS DINNER AT HOME, COME BACK EAT  
+# TODO
+
+refactor the code into folders, helper, service and app.py
--- python-telegram-bot.py
+++ python-telegram-bot.py
@@ -1,7 +1,22 @@
 import logging
-
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram import (
+    ForceReply, 
+    Update, 
+    KeyboardButton,
+    KeyboardButtonPollType,
+    Poll,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,)
+from telegram.constants import ParseMode
+from telegram.ext import (
+    Application, 
+    CommandHandler, 
+    ContextTypes, 
+    MessageHandler, 
+    filters, 
+    PollAnswerHandler,
+    PollHandler,
+    )
 
 from dotenv import load_dotenv
 import os
@@ -14,10 +29,12 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 # set higher logging level for httpx to avoid all GET and POST requests being logged
-logging.getLogger("httpx").setLevel(logging.WARNING)
+# logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
+# TODO, dont hard code this number 
+TOTAL_VOTER_COUNT = 1
 
 # Define a few command handlers. These usually take the two arguments update and
 # context.
@@ -34,6 +51,66 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     """Send a message when the command /help is issued."""
     await update.message.reply_text("Help!")
 
+async def poll(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Sends a predefined poll"""
+    questions = ["Good", "Really good", "Fantastic", "Great"]
+    message = await context.bot.send_poll(
+        update.effective_chat.id,
+        "How are you?",
+        questions,
+        is_anonymous=False,
+        allows_multiple_answers=True,
+    )
+    # Save some info about the poll the bot_data for later use in receive_poll_answer
+    payload = {
+        message.poll.id: {
+            "questions": questions,
+            "message_id": message.message_id,
+            "chat_id": update.effective_chat.id,
+            "answers": 0,
+        }
+    }
+    context.bot_data.update(payload)
+
+async def receive_poll_answer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Summarize a users poll vote"""
+    answer = update.poll_answer
+    answered_poll = context.bot_data[answer.poll_id]
+    try:
+        questions = answered_poll["questions"]
+    # this means this poll answer update is from an old poll, we can't do our answering then
+    except KeyError:
+        return
+    selected_options = answer.option_ids
+    answer_string = ""
+    for question_id in selected_options:
+        if question_id != selected_options[-1]:
+            answer_string += questions[question_id] + " and "
+        else:
+            answer_string += questions[question_id]
+    await context.bot.send_message(
+        answered_poll["chat_id"],
+        f"{update.effective_user.mention_html()} feels {answer_string}!",
+        parse_mode=ParseMode.HTML,
+    )
+    answered_poll["answers"] += 1
+    # Close poll after three participants voted
+    if answered_poll["answers"] == TOTAL_VOTER_COUNT:
+        await context.bot.stop_poll(answered_poll["chat_id"], answered_poll["message_id"])
+
+
+async def receive_poll(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """On receiving polls, reply to it by a closed poll copying the received poll"""
+    actual_poll = update.effective_message.poll
+    # Only need to set the question and options, since all other parameters don't matter for
+    # a closed poll
+    await update.effective_message.reply_poll(
+        question=actual_poll.question,
+        options=[o.text for o in actual_poll.options],
+        # with is_closed true, the poll/quiz is immediately closed
+        is_closed=True,
+        reply_markup=ReplyKeyboardRemove(),
+    )
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
@@ -48,6 +125,9 @@ def main() -> None:
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("poll", poll))
+    application.add_handler(MessageHandler(filters.POLL, receive_poll))
+    application.add_handler(PollAnswerHandler(receive_poll_answer))
 
     # on non command i.e message - echo the message on Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
